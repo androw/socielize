b10793731c60534d97910457f6db289853dfc4e7
Nicolas Lorin <androw95220@gmail.com>
2014-08-15 10:57:33 +0200

    Write Password Encoder

--- src/main/java/com/androw/socielize/config/SecurityConfig.java
+++ src/main/java/com/androw/socielize/config/SecurityConfig.java
@@ -5,7 +5,9 @@ package com.androw.socielize.config;
  */
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
@@ -19,5 +21,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth
                 .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
+    }
+
+    public @Bean
+    ShaPasswordEncoder passwordEncoder() {
+        ShaPasswordEncoder pass = new ShaPasswordEncoder(256);
+        return pass;
     }
 }
